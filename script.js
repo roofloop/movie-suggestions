d3a6b2e6f81ee5af8f47c19249a13463b4fb68f5
Semir Hodzic <semir.hodzic@teledyneflir.com>
2023-09-01 18:03:20 +0200

    wip

--- script.js
+++ script.js
@@ -3,13 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
   const movieInput = document.getElementById("movieInput");
   const movieList = document.getElementById("movieList");
 
+  // Load existing movies from localStorage
+  const movies = JSON.parse(localStorage.getItem("movies")) || [];
+
+  // Function to update movie list and localStorage
+  function updateMovies() {
+    movieList.innerHTML = "";
+    movies.forEach((movie, index) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = movie;
+
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", function() {
+        movies.splice(index, 1);
+        updateMovies();
+      });
+
+      listItem.appendChild(removeButton);
+      movieList.appendChild(listItem);
+    });
+
+    // Update localStorage
+    localStorage.setItem("movies", JSON.stringify(movies));
+  }
+
+  // Initialize movie list
+  updateMovies();
+
+  // Add a new movie
   movieForm.addEventListener("submit", function(e) {
     e.preventDefault();
-    const movieName = movieInput.value;
-    if (movieName) {
-      const listItem = document.createElement("li");
-      listItem.textContent = movieName;
-      movieList.appendChild(listItem);
+    const movieName = movieInput.value.trim();
+    if (movieName && !movies.includes(movieName)) {
+      movies.push(movieName);
+      updateMovies();
     }
+    movieInput.value = "";
   });
 });
